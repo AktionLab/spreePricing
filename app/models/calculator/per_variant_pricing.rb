2cc4348ad3c038bf6958515ff06704a7aee8b654
Andrew Hooker <adhlssu07@gmail.com>
2011-04-15 21:43:28 -0500

    fixing a bad commit

--- app/models/calculator/per_variant_pricing.rb
+++ app/models/calculator/per_variant_pricing.rb
@@ -10,7 +10,6 @@ class Calculator::PerVariantPricing < Calculator
   end
 
   def compute(object)
-<<<<<<< HEAD
 
     return unless object.present? and object.line_items.present? and object.user.present? and object.user.user_group.present?
 
@@ -26,30 +25,5 @@ class Calculator::PerVariantPricing < Calculator
   
   def compute_item(variant)
       variant.price_for_user(@current_user)
-=======
-    return unless object.present? and object.line_items.present? and object.user.present?
-
-    part = self.preferred_flat_percent.abs / 100.0
-    item_total = object.line_items.map(&:amount).sum
-    
-    item_cost_price_total = object.line_items.map do |li| 
-        li.variant.price_for_user(object.user_group) * li.quantity
-        
-      end.sum
-  end
-  
-  def compute_item(variant)
-    part = self.preferred_flat_percent.abs / 100.0
-    
-    if self.preferred_based_on_cost_price
-      if !variant.cost_price.nil? && variant.cost_price > 0
-        variant.cost_price * (1 + part)
-      else
-        variant.price
-      end
-    else
-      variant.price * (1 - part)
-    end
->>>>>>> 15845eea5165d61cd5eb6856b3fac5cffb421a61
   end
 end
