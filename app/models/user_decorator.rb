4faefa85b7823bc20861318ca3f2077de6ec80de
Andrew Hooker <adhlssu07@gmail.com>
2011-06-04 20:57:07 -0500

    removing Adjustment for pricing

--- app/models/user_decorator.rb
+++ app/models/user_decorator.rb
@@ -3,8 +3,6 @@ User.class_eval do
   attr_accessible :user_group_id
   
   def create_adjustment_for(order)
-    if self.user_group && order.adjustments.where(:originator_type => "UserGroup").count == 0
-      self.user_group.create_adjustment(I18n.t(:user_group_adjustment), order, order, true)
-    end
+     return false
   end
 end
